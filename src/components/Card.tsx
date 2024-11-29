238f68e98068ee52a79bc4cc2f821755fbce89ac
avigupta250 <avinashkumarnav52@gmail.com>
2024-11-29 19:15:47 +0530

    you tube baseiurl modified

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -31,7 +31,7 @@ export function Card(props: CardProps) {
         <div className=" rounded-md pt-3">
             {
                 (props.type == "youtube") ? <div>
-                    <iframe width="full" height="full" src={props.link.replace("watch","embed").replace("?v=","/")} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                    <iframe width="full" height="full" src={`https://www.youtube.com/watch?v=${props.link.split("youtu.be/")[1].split("?")[0]}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
                 </div> : 
                 <div className="h-[200px] overflow-y-scroll">
@@ -45,3 +45,7 @@ export function Card(props: CardProps) {
 
     </div>
 }
+// `https://www.youtube.com/watch?v=${props.link.split("youtu.be/")[1].split("?")[0]}`
+
+// https://youtu.be/yS8k-bWtMWk?si=WUu9TE0S_o_uK1DA
+// https://www.youtube.com/watch?v=yS8k-bWtMWk
